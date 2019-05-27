11471d16d04e6a4bfa8cc3c5ef04ad5876aea220
Jeremy Pollock <jpollock911@gmail.com>
2019-05-27 05:58:08 -0700

    cleaning up branch

---
+++ src/core/endpoints/uuid_metadata/add_channel_metadata.js
@@ -0,0 +1,64 @@
+/* @flow */
+
+import { AddChannelParams, ModulesInject } from '../../flow_interfaces';
+import operationConstants from '../../constants/operations';
+import utils from '../../utils';
+
+export function usePost(): boolean {
+  return true;
+}
+
+export function getOperation(): string {
+  return operationConstants.PNUuidMetadataCreate;
+}
+
+export function validateParams(modules: ModulesInject, incomingParams: CreateUuidMetadataParams) {
+  let { config } = modules;
+
+  if (!config.subscribeKey) return 'Missing Subscribe Key';
+}
+
+export function postURL(modules: ModulesInject, incomingParams: CreateUuidMetadataParams): string {
+  let { config } = modules;
+
+  return `/v1/data/sub-key/${config.subscribeKey}/spaces`;
+}
+
+export function getRequestTimeout({ config }: ModulesInject): number {
+  return config.getTransactionTimeout();
+}
+
+export function isAuthSupported(): boolean {
+  return true;
+}
+
+export function prepareParams(modules: ModulesInject, incomingParams: CreateUuidMetadataParams): Object {
+  let { name, description} = incomingParams;
+  const params = {};
+
+  //params.state = JSON.stringify(state);
+
+  return params;
+}
+
+function prepareMessagePayload(modules, messagePayload) {
+  const { crypto, config } = modules;
+  let stringifiedPayload = JSON.stringify(messagePayload);
+
+  if (config.cipherKey) {
+    stringifiedPayload = crypto.encrypt(stringifiedPayload);
+    stringifiedPayload = JSON.stringify(stringifiedPayload);
+  }
+
+  return stringifiedPayload;
+}
+
+export function postPayload(modules: ModulesInject, incomingParams: CreateUuidMetadataParams): string {
+  return prepareMessagePayload(modules, incomingParams);
+}
+
+
+export function handleResponse(modules: ModulesInject, serverResponse: Object): Object {
+  return serverResponse;
+}
+
